f63504f49013211a5e7880186d9dbb1f80af7c26
Andrey Malkov <Andrey.Malkov@quest.com>
2011-11-28 09:21:41 +0300

    configure

--- src/settings.py
+++ src/settings.py
@@ -112,8 +112,9 @@ INSTALLED_APPS = (
 
 info_portal_tests = 'info-portal-tests'
 info_portal_path = os.path.join('sharepoint information portal', 'application', 'tests', 'cases')
+riurik_tests_path = os.path.join('tests', 'riurik', 'cases')
 VIRTUAL_PATHS = {
-	'info-portal-tests': os.path.join('c:\\hunter', info_portal_path),
+	'info-portal-tests': os.path.join('c:\\hunter', riurik_tests_path),
 	'warrior': os.path.join('c:\\warrior', info_portal_path),
 	'wizard': os.path.join('c:\\wizard', info_portal_path),
 	'blade': os.path.join('c:\\blade', info_portal_path),
